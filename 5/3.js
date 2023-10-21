b3517a83ed53641c495a394c7ceddd4787894b28
AnnRazareniva <145858547+AnnRazareniva@users.noreply.github.com>
2023-10-21 11:03:17 +0700

    Add files via upload

---
+++ 5/3.js
@@ -0,0 +1,18 @@
+var new1=document.querySelectorAll(.pop)[0];
+var new2=document.querySelectorAll(.pop)[1];
+var new3=document.querySelectorAll(.pop)[2];
+
+function News1()
+{
+	new1.style.display='block';
+}
+
+function News2()
+{
+	new2.style.display='block';
+}
+
+function News3()
+{
+	new3.style.display='block';
+}
